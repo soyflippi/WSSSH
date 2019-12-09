8a1d8d60657165be4b20f5cf5c9f7a0864414d3a
flippi <soyflippi@gmail.com>
2019-12-09 13:23:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* var express = require('express');
+var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -120,14 +120,3 @@ io.on('connection', function(socket) {
         }
     });
 });
-*/
-const socksv5 = require('socksv5')
-
-const PORT = 1080
-const server = socksv5.createServer((_, resolve) => resolve())
-
-server.listen(PORT, () => console.log(`Listen: localhost:${PORT}`))
-server.useAuth(socksv5.auth.UserPassword((username, password, cb) => {
-  console.log(`Connected a new user: ${username} ${password}`)
-  cb(true)
-}));
