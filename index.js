e93eeb545f486337f3f86510477cf3fbf2635cee
flippi <soyflippi@gmail.com>
2019-12-09 13:19:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -121,15 +121,13 @@ io.on('connection', function(socket) {
     });
 });
 */
-var socks = require('socksv5');
+const socksv5 = require('socksv5')
 
-var srv = socks.createServer(function(info, accept, deny) {
-  accept();
-});
-srv.listen(process.env.PORT, 'localhost', function() {
-  console.log('SOCKS server listening on port ' + process.env.PORT);
-});
+const PORT = 1080
+const server = socksv5.createServer((_, resolve) => resolve())
 
-srv.useAuth(socks.auth.UserPassword(function(user, password, cb) {
-  cb(user === 'flippi' && password === '1234');
+server.listen(PORT, () => console.log(`Listen: localhost:${PORT}`))
+server.useAuth(socksv5.auth.UserPassword((username, password, cb) => {
+  console.log(`Connected a new user: ${username} ${password}`)
+  cb(true)
 }));
